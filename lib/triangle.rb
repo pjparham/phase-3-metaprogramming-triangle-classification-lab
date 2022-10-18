abe627cde6896f117f03ff308ad8942a9dc8fb76
Parker Parham <parkerparham@gmail.com>
2022-10-18 12:40:52 -0400

    commit tree 206486d47343ce1dc5c6c96d978082de4233e5bf

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,48 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if self.has_size? && self.equality?
+      if side1 == side2 && side2 == side3 && side1 == side3
+        :equilateral
+      elsif side1 == side2 || side2 == side3 || side1 == side3
+        :isosceles
+      elsif side1 != side2 && side2 != side3 && side1 != side3
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+
+  def has_size?
+   if side1 > 0 && side2 > 0 && side3 > 0 
+      true
+    else
+      false
+    end 
+  end
+
+  def equality?
+    if side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2
+      true
+    else 
+      false
+    end
+  end
+
+
+  class TriangleError < StandardError
+  end
+
 end
+
+
